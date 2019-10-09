6bd1b22719433271e20dabbcb383659d17c88da5
Ben Fidge <ben.fidge@btopenworld.com>
2019-10-09 11:16:31 +0700

    Added integration tests

---
+++ test/integration/ExternalSearch.KnowledgeGraph.Integration.Tests/KnowledgeGraphTests.cs
@@ -0,0 +1,105 @@
+﻿namespace ExternalSearch.KnowledgeGraph.Integration.Tests
+{
+    public class KnowledgeGraphTests
+    {
+        // private TestContext testContext;
+        // TODO Issue 170 - Unit Test Failures
+
+        // [Fact]
+        // public void Test()
+        // {
+        //     // Arrange
+        //     this.testContext = new TestContext();
+        //     IEntityMetadata entityMetadata = new EntityMetadataPart()
+        //     {
+        //         Name = "Microsoft",
+        //         EntityType = EntityType.Organization
+        //     };
+
+        //     var externalSearchProvider = new Mock<KnowledgeGraphExternalSearchProvider>(MockBehavior.Loose);
+        //     var clues = new List<CompressedClue>();
+
+        //     externalSearchProvider.CallBase = true;
+
+        //     this.testContext.ProcessingHub.Setup(h => h.SendCommand(It.IsAny<ProcessClueCommand>())).Callback<IProcessingCommand>(c => clues.Add(((ProcessClueCommand)c).Clue));
+        //     this.testContext.Container.Register(Component.For<IExternalSearchProvider>().UsingFactoryMethod(() => externalSearchProvider.Object));
+
+        //     var context = this.testContext.Context.ToProcessingContext();
+        //     var command = new ExternalSearchCommand();
+        //     var actor = new ExternalSearchProcessingAccessor(context.ApplicationContext);
+        //     var workflow = new Mock<Workflow>(MockBehavior.Loose, context, new EmptyWorkflowTemplate<ExternalSearchCommand>());
+
+        //     workflow.CallBase = true;
+
+        //     command.With(context);
+        //     command.OrganizationId = context.Organization.Id;
+        //     command.EntityMetaData = entityMetadata;
+        //     command.Workflow = workflow.Object;
+        //     context.Workflow = command.Workflow;
+
+        //     // Act
+        //     var result = actor.ProcessWorkflowStep(context, command);
+        //     Assert.Equal(WorkflowStepResult.Repeat.SaveResult, result.SaveResult);
+
+        //     result = actor.ProcessWorkflowStep(context, command);
+        //     Assert.Equal(WorkflowStepResult.Success.SaveResult, result.SaveResult);
+        //     context.Workflow.AddStepResult(result);
+
+        //     context.Workflow.ProcessStepResult(context, command);
+
+        //     // Assert
+        //     this.testContext.ProcessingHub.Verify(h => h.SendCommand(It.IsAny<ProcessClueCommand>()), Times.AtLeastOnce);
+
+        //     Assert.True(clues.Count > 0);
+        // }
+
+        // TODO Issue 170 - Unit Test Failures
+        //[Fact]
+        //public void Test2()
+        //{
+        //    // Arrange
+        //    this.testContext = new TestContext();
+        //    IEntityMetadata entityMetadata = new EntityMetadataPart()
+        //    {
+        //        Name = "Bang & Olufsen",
+        //        EntityType = EntityType.Organization
+        //    };
+
+        //    var externalSearchProvider = new Mock<KnowledgeGraphExternalSearchProvider>(MockBehavior.Loose);
+        //    var clues = new List<CompressedClue>();
+
+        //    externalSearchProvider.CallBase = true;
+
+        //    this.testContext.ProcessingHub.Setup(h => h.SendCommand(It.IsAny<ProcessClueCommand>())).Callback<IProcessingCommand>(c => clues.Add(((ProcessClueCommand)c).Clue));
+        //    this.testContext.Container.Register(Component.For<IPropertyTranslationService>().UsingFactoryMethod(() => new PropertyTranslationService()));
+
+        //    var context = this.testContext.Context.ToProcessingContext();
+        //    var command = new ExternalSearchCommand();
+        //    var actor = new ExternalSearchProcessing(context.ApplicationContext);
+        //    var workflow = new Mock<Workflow>(MockBehavior.Loose, context, new EmptyWorkflowTemplate<ExternalSearchCommand>());
+
+        //    workflow.CallBase = true;
+
+        //    command.With(context);
+        //    command.OrganizationId = context.Organization.Id;
+        //    command.EntityMetaData = entityMetadata;
+        //    command.Workflow = workflow.Object;
+        //    context.Workflow = command.Workflow;
+
+        //    // Act
+        //    var result = actor.ProcessWorkflowStep(context, command);
+        //    Assert.Equal(WorkflowStepResult.Repeat.SaveResult, result.SaveResult);
+
+        //    result = actor.ProcessWorkflowStep(context, command);
+        //    Assert.Equal(WorkflowStepResult.Success.SaveResult, result.SaveResult);
+        //    context.Workflow.AddStepResult(result);
+
+        //    context.Workflow.ProcessStepResult(context, command);
+
+        //    // Assert
+        //    this.testContext.ProcessingHub.Verify(h => h.SendCommand(It.IsAny<ProcessClueCommand>()), Times.AtLeastOnce);
+
+        //    Assert.True(clues.Count > 0);
+        //}
+    }
+}
---
+++ test/integration/ExternalSearch.KnowledgeGraph.Integration.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e4418764-f1ef-40d3-aa8c-c2c6c38b6654")]
