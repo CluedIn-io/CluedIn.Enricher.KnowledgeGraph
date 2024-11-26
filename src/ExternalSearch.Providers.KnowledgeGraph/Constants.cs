884cfa95985a95597c8740914af2fce069a92cbf
Yi-Wei <86753236+ywei390@users.noreply.github.com>
2024-11-26 14:02:30 +0800

    feat: Update enricher configuration page controls to selector (#30)

--- src/ExternalSearch.Providers.KnowledgeGraph/Constants.cs
+++ src/ExternalSearch.Providers.KnowledgeGraph/Constants.cs
@@ -69,7 +69,7 @@ namespace CluedIn.ExternalSearch.Providers.KnowledgeGraph
                 new Control()
                 {
                     DisplayName = "Accepted Entity Type",
-                    Type = "input",
+                    Type = "entityTypeSelector",
                     IsRequired = true,
                     Name = KeyName.AcceptedEntityType,
                     Help = "The entity type that defines the golden records you want to enrich (e.g., /Organization)."
@@ -77,7 +77,7 @@ namespace CluedIn.ExternalSearch.Providers.KnowledgeGraph
                 new Control()
                 {
                     DisplayName = "Organization Name Vocabulary Key",
-                    Type = "input",
+                    Type = "vocabularyKeySelector",
                     IsRequired = false,
                     Name = KeyName.OrganizationNameKey,
                     Help = "The vocabulary key that contains the names of companies you want to enrich (e.g., organization.name)."
@@ -85,7 +85,7 @@ namespace CluedIn.ExternalSearch.Providers.KnowledgeGraph
                 new Control()
                 {
                     DisplayName = "Website Vocabulary Key",
-                    Type = "input",
+                    Type = "vocabularyKeySelector",
                     IsRequired = false,
                     Name = KeyName.WebsiteKey,
                     Help = "The vocabulary key that contains the websites of companies you want to enrich (e.g., organization.website)."
